26112004a9fe50fd77d16d9821aa2e404e301be9
ofeks <ofekn271@gmail.com>
2020-04-12 14:49:08 +0300

    Updates

---
+++ precache-manifest.5088fe3b31aeb5761997fbe02546248d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e29b23cd5d5f68456b4ea2bf5e4838",
+    "url": "/oksr/react-first-app/index.html"
+  },
+  {
+    "revision": "a36c8a6b85a9be0db77a",
+    "url": "/oksr/react-first-app/static/css/main.24ba2fc9.chunk.css"
+  },
+  {
+    "revision": "0a9f16e1178166525bc3",
+    "url": "/oksr/react-first-app/static/js/2.cb9dd1e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oksr/react-first-app/static/js/2.cb9dd1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36c8a6b85a9be0db77a",
+    "url": "/oksr/react-first-app/static/js/main.a99ba3c7.chunk.js"
+  },
+  {
+    "revision": "38026d77d9418f513b84",
+    "url": "/oksr/react-first-app/static/js/runtime-main.3d04bde6.js"
+  }
+]);
